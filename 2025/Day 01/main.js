9883b795d6e09bfe3b0ae721115788bf1b351cdf
Jonas Stumpf <jonas@jonas-stumpf.de>
2025-12-03 22:30:50 +0100

    day1

---
+++ 2025/Day 01/main.js
@@ -0,0 +1,41 @@
+const fs = require('node:fs');
+const input = fs.readFileSync(__dirname+'/input.txt', 'utf8');
+
+const list = input.split(/\s+/);
+
+let currPos = 50;
+let pass = 0;
+
+for (const item of list) {
+    const [dir, count] = [item[0], parseInt(item.slice(1))];
+    currPos += dir === "L" ? -count : count;
+    
+    if (currPos < 0) currPos += 100;
+    currPos %= 100;
+
+    if (currPos === 0) pass++;
+    
+}
+
+console.log('Password:', pass);
+
+
+/* Part 2 */
+let passAny = 0;
+currPos = 50;
+
+for (const item of list) {
+    const [dir, count] = [item[0], parseInt(item.slice(1))];
+    
+    const step = dir === "L" ? -1 : 1;
+    
+    for (let i = 0; i < count; i++) {
+        currPos += step;
+        if (currPos < 0) currPos += 100;
+        currPos %= 100;
+        if (currPos === 0) passAny++;
+    }
+
+}
+
+console.log('Password any pass:', passAny);
