2b7b027f9c3c87fcef675ef5a3d52bb08312f9f0
Jonas Stumpf <jonas@jonas-stumpf.de>
2025-12-07 20:04:58 +0100

    day 3

--- 2025/Day 03/main.js
+++ 2025/Day 03/main.js
@@ -1,24 +1,40 @@
 const fs = require('node:fs');
 const input = fs.readFileSync(__dirname+'/input.txt', 'utf8');
 
-const lines = input.split('\n');
+const lines = input.split('\n').map(l => l.trim());
 
-let sum = 0;
+let sum2 = 0;
+let sum12 = 0;
 for (const line of lines) {
-    const num = [0, 0];
-
-    for (let i = 0; i < line.length; i++) {
-        const curr = line[i];
-
-        if (curr > num[0] && i < line.length - 2) {
-            num[0] = curr;
-            num[1] = 0;
-        } else if (curr > num[1]) {
-            num[1] = curr;
-        }
-
-    }
-    sum += +(num[0] + num[1]);
+    sum2 += getLineNum(line, 2);
+    sum12 += getLineNum(line, 12);
 }
 
-console.log(sum);
+console.log(sum2);
+
+console.log(sum12);
+
+function getLineNum(line, numLength) {
+    const num = [];
+    num.length = numLength;
+    num.fill(0);
+
+    for (let i = 0; i < line.length; i++) { //every digit in line
+        const curr = line[i];
+        
+        const rest = line.length - i;
+        const start = Math.max(0, numLength - rest);
+        
+        for (let j = start; j < numLength; j++) { //check num list
+            if (curr > num[j]) {
+                num[j] = curr;
+                num.fill(0, j + 1);
+                break;
+            }
+        }
+
+
+    }
+    
+    return +num.join('');
+}
